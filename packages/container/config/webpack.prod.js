9d05ed2bea1383b038ccca60c0e53773520926e4
Isaac Vicliph <Isaac.Vicliph@firstcu.co.nz>
2021-01-28 18:38:39 +1300

    added public path

--- packages/container/config/webpack.prod.js
+++ packages/container/config/webpack.prod.js
@@ -13,6 +13,7 @@ const prodConfig = {
   mode: 'production',
   output: {
     filename: '[name].[contenthash].js',
+    publicPath: '/container/latest/',
   },
   plugins: [
     new ModuleFederationPlugin({
